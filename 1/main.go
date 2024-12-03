a783b844bf26aea69acb262d978f79642b0d1819
Rushi Panchariya <rushipanchariya19@gmail.com>
2024-12-03 12:39:44 +0530

    adds day 1

--- 1/main.go
+++ 1/main.go
@@ -1,121 +1,85 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
+	"math"
 	"os"
-	"regexp"
+	"sort"
 	"strconv"
+	"strings"
 )
 
-var letters map[string]string
-
-// Part 1: 55621
-// Part 2: 53592
+// Part 1: 2756096
+// Part 2: 23117829
 
 func main() {
-	letters = map[string]string{
-		"one":   "1",
-		"two":   "2",
-		"three": "3",
-		"four":  "4",
-		"five":  "5",
-		"six":   "6",
-		"seven": "7",
-		"eight": "8",
-		"nine":  "9",
-		"eno":   "1",
-		"owt":   "2",
-		"eerht": "3",
-		"ruof":  "4",
-		"evif":  "5",
-		"xis":   "6",
-		"neves": "7",
-		"thgie": "8",
-		"enin":  "9",
-	}
-	//	testInput := `1abc2
-	//
-	// pqr3stu8vwx
-	// a1b2c3d4e5f
-	// treb7uchet`
-	// 	testInput := `two1nine
-	// eightwothree
-	// abcone2threexyz
-	// xtwone3four
-	// 4nineeightseven2
-	// zoneight234
-	// 7pqrstsixteen`
-
 	// 	testSlice := strings.Split(testInput, "\n")
-	testSlice := make([]string, 0)
-	file, err := os.Open("input.txt")
-	if err != nil {
-		panic(err)
-	}
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		testSlice = append(testSlice, scanner.Text())
-	}
-
-	err = file.Close()
+	data, err := os.ReadFile("input.txt")
 	if err != nil {
 		panic(err)
 	}
 
-	if scanner.Err() != nil {
-		panic(scanner.Err())
-	}
+	dataString := string(data)
 
-	// sliceOfTwoDigitNum := make([]int, 0)
+	var arr1, arr2 []int
+
+	for i, v := range strings.Fields(dataString) {
+		val, err := strconv.Atoi(v)
+		if err != nil {
+			panic(err)
+		}
+		if i%2 == 0 {
+			arr1 = append(arr1, val)
+		} else {
+			arr2 = append(arr2, val)
+		}
+	}
+	part1(arr1, arr2)
+
+	part2(arr1, arr2)
+}
+
+func part1(arr1 []int, arr2 []int) {
+	sort.Ints(arr1)
+	sort.Ints(arr2)
+
 	sum := 0
-	for _, d := range testSlice {
-		// sum += extractFirstLastDigit(d)
-		sum += extractDigitFromLetter(d)
+
+	for i := range len(arr1) {
+		temp := arr1[i] - arr2[i]
+		sum += int(math.Abs(float64(temp)))
 	}
+
 	fmt.Println(sum)
 }
 
-func extractDigitFromLetter(s string) int {
-	re_front := regexp.MustCompile(`(\d|one|two|three|four|five|six|seven|eight|nine)`)
-	re_back := regexp.MustCompile(`(\d|eno|owt|eerht|ruof|evif|xis|neves|thgie|enin)`)
-	front_matches := re_front.FindAllString(s, 1)
-	back_mathches := re_back.FindAllString(reverse(s), 1)
-	if len(front_matches) != 0 {
-		twoDigits := checkInMap(front_matches[0]) + checkInMap(back_mathches[0])
-		digits, err := strconv.ParseInt(twoDigits, 10, 0)
-		if err != nil {
-			fmt.Printf("Error parsing into int: %v", err)
+type valOccurrs struct {
+	val     int
+	occurrs int
+}
+
+func part2(arr1, arr2 []int) {
+	m := make(map[int]valOccurrs)
+
+	for _, val := range arr1 {
+		if d, ok := m[val]; ok {
+			m[val] = valOccurrs{val: 0, occurrs: d.occurrs + 1}
+		} else {
+			m[val] = valOccurrs{0, 0}
 		}
-		return int(digits)
 	}
-	return 0
-}
 
-func reverse(s string) string {
-	runes := []rune(s)
-	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
-		runes[i], runes[j] = runes[j], runes[i]
-	}
-	return string(runes)
-}
-
-func checkInMap(s string) string {
-	if val, ok := letters[s]; ok {
-		return val
-	}
-	return s
-}
-
-func extractFirstLastDigit(s string) int {
-	re := regexp.MustCompile(`\d`)
-	match := re.FindAllString(s, -1)
-	if len(match) != 0 {
-		digits, err := strconv.ParseInt(match[0]+match[len(match)-1], 10, 0)
-		if err != nil {
-			fmt.Printf("Error parsing into int: %v", err)
+	for _, v := range arr2 {
+		if d, ok := m[v]; ok {
+			m[v] = valOccurrs{d.val + 1, d.occurrs}
 		}
-		return int(digits)
 	}
-	return 0
+
+	prod := 0
+	for key, v := range m {
+		for range v.occurrs + 1 {
+			prod += key * v.val
+		}
+	}
+	fmt.Println(prod)
 }
